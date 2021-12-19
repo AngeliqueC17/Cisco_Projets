e0a98225d97d53442500fa99625a8c9778dcf39c
Angélique Coune <75845426+AngeliqueC17@users.noreply.github.com>
2021-12-19 16:20:23 +0100

    Ajout de la solution Bataille et le projet API

---
+++ C#/Bataille/Deck_of_Cards_API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deck_of_Cards_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
