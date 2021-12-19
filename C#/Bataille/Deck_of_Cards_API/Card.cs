c4ac82aa8c49ab0f3fd1fd07d7c75fc13fd38596
Angélique Coune <75845426+AngeliqueC17@users.noreply.github.com>
2021-12-19 18:44:27 +0100

    MAJ classe Card et Deck

--- C#/Bataille/Deck_of_Cards_API/Card.cs
+++ C#/Bataille/Deck_of_Cards_API/Card.cs
@@ -1,9 +1,16 @@
-﻿namespace Deck_of_Cards_API
+﻿using Newtonsoft.Json;
+
+namespace Deck_of_Cards_API
 {
     public class Card
     {
+        [JsonProperty("code")]
         public string Code { get; set; }
+        
+        [JsonProperty("value")]
         public string Value { get; set; }
+        
+        [JsonProperty("suit")]
         public string Suit { get; set; }
 
         public Card(string code, string value, string suit)
--- C#/Bataille/Deck_of_Cards_API/Deck.cs
+++ C#/Bataille/Deck_of_Cards_API/Deck.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Deck_of_Cards_API
 {
     public class Deck
     {
+        [JsonProperty("deck_id")]
         public string ID { get; set; }
+        
+        [JsonProperty("remaining")]
         public int Remaining { get; set; }
+        
+        [JsonProperty("shuffled")]
         public bool Shuffled { get; set; }
+        
+        [JsonProperty("cards")]
         public Stack<Card> Cards {get; set;}
         
         public Deck(string id, int remaining, bool shuffled, Stack<Card> cards)
