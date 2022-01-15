e042b164b0423bddb3af2e2fd821616db30ffe54
Angélique Coune <75845426+AngeliqueC17@users.noreply.github.com>
2022-01-15 15:51:11 +0100

    Récupération du (des) gagnant(s) d'une manche

--- C#/Bataille/Game_Logic/Game_ManagerJulien.cs
+++ C#/Bataille/Game_Logic/Game_ManagerJulien.cs
@@ -1,25 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 using Deck_of_Cards;
 
 namespace Game_Logic
 {
     public static class Game_ManagerJulien
     {
+        private static string[] valeurCarte = new[] {"2", "3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"};
         public static void Manche(List<Player> list)
         {
-            Stack<Card> pile = new Stack<Card>();
+            List<Card> pile = new List<Card>();
             foreach (var joueur in list)
             {
                 Card carte = joueur.Cards.Pop();
                 Console.WriteLine("Le joueur " + joueur.Numero + " a joué un " + carte.Value);
-                pile.Push(carte);
+                pile.Add(carte);
             }
 
-
+            List<Player> winners = Winners(list, pile);
+            
+            if (winners.Count == 1)
+            {
+                Console.WriteLine("Le gagnant est le joueur " + winners[0].Numero);
+            }
+            else
+            {
+                Bataille(winners);
+            }
+        }
+        public static int[] FindAllIndexof<T>(this IEnumerable<T> values, T val)
+        {
+            return values.Select((b,i) => object.Equals(b, val) ? i : -1).Where(i => i != -1).ToArray();
         }
 
-        public static void Bataille()
+        public static List<Player> Winners(List<Player> playersTotal, List<Card> cardsPlayed)
+        {
+            List<int> cardsRanks = new List<int>();
+            
+            //On fait correspondre les cartes jouées selon leur rang dans le tableau valeurCarte
+            foreach (var carte in cardsPlayed)
+            {
+                cardsRanks.Add(Array.FindIndex(valeurCarte, card => card == carte.Value));
+            }
+            
+            //On récupère l'indice de la carte la plus forte d'après le tableau valeurCarte
+            var max = cardsRanks.Max();
+            
+            //On récupère l'indice des joueurs ayant joué la carte la plus forte
+            var t = cardsRanks.FindAllIndexof(max);
+            
+            //On retounr le liste des joueurs avec la plus forte carte jouée
+            return t.Select(t1 => playersTotal[t1]).ToList();
+        }
+        public static void Bataille(List<Player> pq)
         {
             
         }
